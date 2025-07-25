09c1f92626b83ddc1fd458d59b9d7d3150a9b189
Joao Pivatto <jgpivatto@gmail.com>
2025-07-25 10:32:52 -0300

    fix: creating an AuthenticationException to /login endpoint

--- API-REST/src/main/java/com/joao/pivatto/apirest/controller/LoginController.java
+++ API-REST/src/main/java/com/joao/pivatto/apirest/controller/LoginController.java
@@ -5,13 +5,17 @@ import com.joao.pivatto.apirest.infrastructure.TokenService;
 import com.joao.pivatto.apirest.model.Login;
 import com.joao.pivatto.apirest.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/login")
@@ -24,10 +28,15 @@ public class LoginController {
     private TokenService tokenService;
 
     @PostMapping("")
-    public ResponseEntity<Token> login(@RequestBody Login login) {
+    public ResponseEntity<?> login(@RequestBody Login login) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(login.getLogin(), login.getPassword());
-        var auth = this.authenticationManager.authenticate(usernamePassword);
-        var token = tokenService.generateToken((User) auth.getPrincipal());
-        return ResponseEntity.ok(new Token(token));
+        try {
+            var auth = this.authenticationManager.authenticate(usernamePassword);
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+            return ResponseEntity.ok(new Token(token));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Bad credentials"));
+        }
     }
 }
